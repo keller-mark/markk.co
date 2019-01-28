34f104b47e0ae3e4323e233dc50d4a69b4d9e6cb
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2019-01-27 20:25:20 -0500

    Update home page URLs

--- .vuepress/theme/components/Intro.vue
+++ .vuepress/theme/components/Intro.vue
@@ -3,9 +3,9 @@
         <p>
             I am a junior at the University of Maryland with a major in Computer Science (Data Science Track) and minor in Statistics.
 
-            My interests include <a href="http://imuse.lrgr.io">computational biology</a> and <a href="http://github.com/keller-mark/vue-declarative-plots">data visualization</a>.
+            My interests include <a href="http://imuse.lrgr.io">computational biology</a> and <a href="http://github.com/keller-mark/vueplotlib">data visualization</a>.
 
-            Find me on <a class="social" href="https://github.com/keller-mark">GitHub</a> and <a class="social" href="https://www.linkedin.com/in/mark-keller-514574b0">LinkedIn</a>.
+            Find me on <a class="social" href="https://github.com/keller-mark" target="_blank">GitHub</a>, <a class="social" href="https://beta.observablehq.com/@keller-mark" target="_blank">Observable</a>, or <a class="social" href="https://www.linkedin.com/in/mark-keller-514574b0" target="_blank">LinkedIn</a>.
         </p>
     </div>
 </template>
