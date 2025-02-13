0d63db4f0f985798d16683b02a7617cd3c2f0cfb
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-02-12 20:38:09 -0500

    sc-star

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -13,6 +13,14 @@ import imgUrl from './img/mark.png'
 
 const news = [
   {
+    date: "January 28, 2025",
+    slug: "scstar-preprint-jan-2025",
+    description: (<p>
+     The State of Single-Cell Atlas Data Visualization in the Biological Literature preprint uploaded to <a href="https://osf.io/preprints/osf/yt3xz_v1">OSF Preprints</a>.
+     The preprint is accompanied by a <a href="https://keller-mark.github.io/sc-star-site/">survey browser</a>.
+    </p>),
+  },
+  {
     date: "November 12, 2024",
     slug: "spatialdata-workshop-2024",
     description: (<p>
